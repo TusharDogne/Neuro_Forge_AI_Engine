758dca2e92cd544f6cd0c4ce51f6452b9ce01e63
tushar dogne <tushardogne123@gmail.com>
2026-07-02 03:15:42 +0530

    Look Whole Folder Then Start doing something

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.models.user import User
 from app.db.database import Base, engine
+from app.routes.auth import router
 
 
 Base.metadata.create_all(bind=engine)
@@ -10,7 +11,10 @@ app = FastAPI(
     title="NeuroForge API",
     version="1.0.0"
 )
+app.include_router(router)
 
 @app.get("/")
 def home():
     return {"message": "Welcome to the NeuroForge API!"}
+
+
---
+++ backend/app/routes/auth.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+from app.schemas.user import UserCreate
+
+router = APIRouter()
+
+@router.post("/register")
+def register(user: UserCreate):
+    return user
